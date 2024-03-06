efc090a0dce9abade4f04c417524cebc184a956e
IvanBuzin <ua.buzin@gmail.com>
2024-03-06 22:23:00 +0200

    x

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,18 +1,34 @@
 import css from "./ImageModal.module.css";
+import Modal from "react-modal";
 
-export const ImageModal = ({ content: { description, urls, user, links } }) => {
+export const ImageModal = ({
+  isOpen,
+  isClose,
+  content: { description, urls, user, links },
+}) => {
+  Modal.setAppElement("#root");
+
   const handleDownloadClick = () => {
     window.open(links.download, "_blank");
   };
   return (
-    <>
-      <img className={css.image} src={urls.regular} alt={description} />
-      <div className={css.title}>
-        <p>Author: {user.last_name}</p>
-        <button type="button" onClick={handleDownloadClick}>
-          download
-        </button>
-      </div>
-    </>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className={css.modal}
+      overlayClassName={css.overlay}
+    >
+      {isOpen && (
+        <>
+          <img className={css.image} src={urls.regular} alt={description} />
+          <div className={css.title}>
+            <p>Author: {user.last_name}</p>
+            <button type="button" onClick={handleDownloadClick}>
+              download
+            </button>
+          </div>
+        </>
+      )}
+    </Modal>
   );
 };
